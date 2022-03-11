6ebc06b48534ea9673a3165ccde7372060bf74ee
KunioTerada <73575419+pizzzza-planet@users.noreply.github.com>
2022-03-11 11:19:03 +0900

    React.memoを使った最適化

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import "./styles.css";
+import React, { useState } from "react";
+
+const Count = React.memo(({ text, countState }) => {
+  console.log(countState);
+  return (
+    <p>
+      {text}:{countState}
+    </p>
+  );
+});
 
 export default function App() {
+  const [countStateA, setCountStateA] = useState(0);
+  const [countStateB, setCountStateB] = useState(0);
+
+  const incrementACounter = () => setCountStateA(countStateA + 1);
+  const incrementBCounter = () => setCountStateB(countStateB + 1);
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <>
+        <Count text="A ボタン" countState={countStateA} />
+        <Count text="B ボタン" countState={countStateB} />
+        <button onClick={incrementACounter}>A ボタン</button>
+        <button onClick={incrementBCounter}>B ボタン</button>
+      </>
     </div>
   );
 }
