5382187ad605f7e64f5e1b4baec01646721d3581
KunioTerada <73575419+pizzzza-planet@users.noreply.github.com>
2022-03-11 13:16:10 +0900

    useMemo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./styles.css";
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useState, useMemo } from "react";
 
 const Count = React.memo(({ text, countState }) => {
   console.log(countState);
@@ -15,6 +15,13 @@ const Button = React.memo(({ counterState, buttonValue }) => {
   return <button onClick={counterState}>{buttonValue}</button>;
 });
 
+const square = (parameter) => {
+  console.log("square関数の実行観察");
+  let i = 0;
+  while (i < 10000) i++;
+  return parameter * parameter;
+};
+
 export default function App() {
   const [countStateA, setCountStateA] = useState(0);
   const [countStateB, setCountStateB] = useState(0);
@@ -26,6 +33,16 @@ export default function App() {
     countStateB
   ]);
 
+  const resultA = () => {
+    return setCountStateA(countStateA + 1);
+  };
+
+  const resultB = () => {
+    return setCountStateB(countStateB + 1);
+  };
+
+  const squareArea = useMemo(() => square(countStateB), [countStateB]);
+
   return (
     <div className="App">
       <>
@@ -33,6 +50,17 @@ export default function App() {
         <Count text="B ボタン" countState={countStateB} />
         <Button counterState={incrementACounter} buttonValue="A ボタン" />
         <Button counterState={incrementBCounter} buttonValue="B ボタン" />
+
+        <p>
+          計算結果A: {countStateA}
+          <button onClick={resultA}>計算A + 1</button>
+        </p>
+        <p>【正方形の面積】</p>
+        <p>
+          計算結果B: {countStateB}
+          <button onClick={resultB}>計算B + 1</button>
+        </p>
+        <p>計算結果B　×　計算結果B = {squareArea}</p>
       </>
     </div>
   );
